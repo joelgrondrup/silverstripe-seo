e92509f65bed369b958d9ecd23dc02cc378b3bad
Joel Grøndrup <joel@joel.biz>
2025-12-17 13:09:51 +0100

    Update JavaScript requirement path in constructor

--- src/Forms/MetaPreviewField.php
+++ src/Forms/MetaPreviewField.php
@@ -41,7 +41,7 @@ class MetaPreviewField extends LiteralField
     {
         $this->page = $page;
         
-        Requirements::javascript('cyber-duck/silverstripe-seo:assets/js/serp.js');
+        Requirements::javascript('joelgrondrup/silverstripe-seo:assets/js/serp.js');
 
         parent::__construct('MetaPreviewField', $this->getMetaContent());
     }
